7b41b93fd78bd596bc612007ef1a058183b2ef7e
yanxiaolzay <1977735808@qq.com>
2020-11-10 12:15:55 +0800

    build: remove note folder to src folder and init a react page

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {  
+  GridLayout,
+  DosCommand,
+  BuildAndPush,
+  DockerfileConfigAnalyse,
+  CanalBinlogOutOfSync,
+  DeferAsyncAndDOMContentloaded,
+  ForInAndForOf,
+  KoaAppContext,
+  SequelizeCliUse,
+  YouDonotKnownViod0,
+  NginxConfigAnalyse,
+  ReactWorkingPrinciple,
+  CreatingImmutableObjects,
+  UbuntuExpansion} from './note';
+
+render(
+  <div>
+    <GridLayout/>
+    <hr />
+    <DosCommand/>
+    <hr />
+    <BuildAndPush/>
+    <hr />
+    <DockerfileConfigAnalyse/>
+    <hr />
+    <CanalBinlogOutOfSync/>
+    <hr />
+    <DeferAsyncAndDOMContentloaded/>
+    <hr />
+    <ForInAndForOf/>
+    <hr />
+    <KoaAppContext/>
+    <hr />
+    <SequelizeCliUse/>
+    <hr />
+    <YouDonotKnownViod0/>
+    <hr />
+    <NginxConfigAnalyse/>
+    <hr />
+    <ReactWorkingPrinciple/>
+    <hr />
+    <CreatingImmutableObjects/>
+    <hr />
+    <UbuntuExpansion/>
+    <hr />
+  </div>,
+  document.getElementById('root')
+);
+
---
+++ src/note/index.js
@@ -0,0 +1,40 @@
+import './index.css';
+//css
+import GridLayout from './css/网格布局（grid）.md';
+//dos
+import DosCommand from './dos命令/停止被占用端口.md';
+//docker
+import BuildAndPush from './docker/build and push to Dockr Hub.md';
+import DockerfileConfigAnalyse from './docker/Dockerfile简单配置分析.md';
+import CanalBinlogOutOfSync from './docker/坑：canal的binLog与mysql不同步.md';
+//javascript
+import DeferAsyncAndDOMContentloaded from './js/defer async 和 DOMContentLoaded.md';
+import ForInAndForOf from './js/for-in和for-of.md';
+import KoaAppContext from './js/koa2 app.context.md';
+import SequelizeCliUse from './js/sequelize cli.md';
+import YouDonotKnownViod0 from './js/你不知道的viod 0.md';
+//nginx
+import NginxConfigAnalyse from './nginx/nginx配置文件介绍.md';
+//react
+import ReactWorkingPrinciple from './react/react 工作原理.md';
+//translate
+import CreatingImmutableObjects from './translate/Creating immutable objects in native JavaScript.md';
+//ubuntu
+import UbuntuExpansion from './ubuntu/扩容.md';
+
+export {
+  GridLayout,
+  DosCommand,
+  BuildAndPush,
+  DockerfileConfigAnalyse,
+  CanalBinlogOutOfSync,
+  DeferAsyncAndDOMContentloaded,
+  ForInAndForOf,
+  KoaAppContext,
+  SequelizeCliUse,
+  YouDonotKnownViod0,
+  NginxConfigAnalyse,
+  ReactWorkingPrinciple,
+  CreatingImmutableObjects,
+  UbuntuExpansion
+}
