35fa55295b38632b2ae92e410b24eb8e70e9ec3d
yanxiaolzay <1977735808@qq.com>
2020-11-10 11:34:37 +0800

    build: add webpack config

---
+++ config/webpack.config.js
@@ -0,0 +1,78 @@
+const {resolve} = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+// const remarkGfm = require('remark-gfm');
+
+
+const mode = process.env.NODE_DEV === 'development' ? 'development' : 'production';
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: resolve(__dirname, '../src/index.js')
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: resolve(__dirname, '../build')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 80,
+    hot: true,
+    contentBase: resolve(__dirname, '../build'),
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, 
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+            presets: ['@babel/preset-react']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+        ] 
+      },
+      {
+        test: /\.md$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react']
+            }
+          },
+          {
+            loader: '@mdx-js/loader',
+            options: {
+              // remarkPlugins: [remarkGfm]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
+    new HtmlWebpackPlugin({template: resolve(__dirname, '../public/index.html')})
+  ],
+  resolve: {
+    fallback: {
+      path: require.resolve('path-browserify')
+    }
+  }
+}
