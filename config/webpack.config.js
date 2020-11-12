cffe06a8a34dc7099ebf5d5daaad2a4376fb951b
yanxiaolzay <1977735808@qq.com>
2020-11-12 12:57:39 +0800

    update: add optimization through code separation

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,80 +2,88 @@ const {resolve} = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const remarkGfm = require('remark-gfm');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+const plugins = [
+  new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
+  new HtmlWebpackPlugin({template: resolve(__dirname, '../public/index.html')}),
+]
 
-const mode = process.env.NODE_DEV === 'development' ? 'development' : 'production';
+module.exports = env => {
+  const mode = env.NODE_ENV !== 'production' ? 'development' : 'production';
+  const devMode = !env.production;
 
-module.exports = {
-  mode,
-  entry: {
-    index: resolve(__dirname, '../src/index.js')
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: resolve(__dirname, '../docs')
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    host: '0.0.0.0',
-    port: 80,
-    hot: true,
-    contentBase: resolve(__dirname, '../build'),
-    open: true,
-    inline: true,
-    historyApiFallback: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/, 
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(sass|scss)$/,
-        use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader'
-        ] 
-      },
-      {
-        test: /\.md$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react']
-            }
-          },
-          {
-            loader: '@mdx-js/loader',
-            options: {
-              remarkPlugins: [remarkGfm]
-            }
-          }
-        ]
+  //MiniCssExtractPlugin会使HMR功能缺失
+  if (!devMode) {
+    plugins.push(new MiniCssExtractPlugin({filename: '[name].[contenthash].css'}));
+  }
+
+  return {
+    mode,
+    entry: {
+      index: resolve(__dirname, '../src/index.js')
+    },
+    output: {
+      filename: '[name].[hash].js',
+      path: resolve(__dirname, '../docs')
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        maxSize: 244000
       }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
-    new HtmlWebpackPlugin({template: resolve(__dirname, '../public/index.html')})
-  ],
-  resolve: {
-    fallback: {
-      path: require.resolve('path-browserify')
+    },
+    devtool: devMode ? 'inline-source-map' : false,
+    devServer: {
+      port: 80,
+      hot: true,
+      contentBase: resolve(__dirname, '../docs'),
+      open: true,
+      inline: true,
+      historyApiFallback: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/, 
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+              devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+              'css-loader',
+              'sass-loader'
+          ] 
+        },
+        {
+          test: /\.md$/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+              }
+            },
+            {
+              loader: '@mdx-js/loader',
+              options: {
+                remarkPlugins: [remarkGfm]
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins,
+    resolve: {
+      fallback: {
+        path: require.resolve('path-browserify')
+      }
     }
   }
 }
