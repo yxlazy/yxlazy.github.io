7bac032f3bfe86bbc27035d30e7dc423a641390c
yanxiaolzay <1977735808@qq.com>
2020-11-18 11:57:38 +0800

    perf: add a global.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, {StrictMode, Fragment} from 'react';
 import {render} from 'react-dom';
 import App from './views/App';
-import 'antd/dist/antd.css';
+import './assets/css/global.css';
 
 render(
   <StrictMode>
