f97b75cea53ce9508bab24dc62db43cbb0b6e45a
yanxiaolzay <1977735808@qq.com>
2020-11-19 17:27:34 +0800

    perf: add a 'Card' component

--- src/views/BlogLayout/index.js
+++ src/views/BlogLayout/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useRouteMatch } from 'react-router-dom';
 import * as note from '../../note';
 import Navbar from '../../components/Navbar';
+import { Card } from 'antd';
 
 export default BlogLayout;
 
@@ -14,7 +15,9 @@ function BlogLayout() {
     <div className='blog-layout'>
       <Navbar  />
       <div className='markdown bloglayout'>
-        {Details && <Details/>}
+        <Card>
+          {Details && <Details/>}
+        </Card>
       </div>
     </div>
   );
