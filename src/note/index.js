fc99b6194661db63ce4eee655eaaf9da860d3567
yanxiaolzay <1977735808@qq.com>
2021-01-09 11:34:59 +0800

    fix: 修理'import'导出模块不存在的bug

--- src/note/index.js
+++ src/note/index.js
@@ -1,13 +1,16 @@
 import './index.scss';
 //css
 import GridLayout from './css/网格布局（grid）.md';
+
 //dos
 import DosCommand from './dos命令/停止被占用端口.md';
+
 //docker
 import BuildAndPush from './docker/build and push to Dockr Hub.md';
 import DockerfileConfigAnalyse from './docker/Dockerfile简单配置分析.md';
 import CanalBinlogOutOfSync from './docker/坑：canal的binLog与mysql不同步.md';
-import { InstallDockerENGineAndDockerCompose } from "./docker/docker engine 和docker compose安装.md";
+import InstallDockerENGineAndDockerCompose from "./docker/docker engine 和docker compose安装.md";
+
 //javascript
 import DeferAsyncAndDOMContentloaded from './js/defer async 和 DOMContentLoaded.md';
 import ForInAndForOf from './js/for-in和for-of.md';
@@ -16,14 +19,19 @@ import SequelizeCliUse from './js/sequelize cli.md';
 import YouDonotKnownViod0 from './js/你不知道的viod 0.md';
 import CalleeAndCaller from './js/arguments.callee 和fn.caller.md';
 import ThisObject from './js/this对象.md';
+
 //nginx
 import NginxConfigAnalyse from './nginx/nginx配置文件介绍.md';
+
 //react
 import ReactWorkingPrinciple from './react/react 工作原理.md';
+
 //translate
 import CreatingImmutableObjects from './translate/Creating immutable objects in native JavaScript.md';
+
 //ubuntu
 import UbuntuExpansion from './ubuntu/扩容.md';
+
 // mysql
 import ForgetPassword from './mysql/macbook pro安装mysql后忘记密码.md';
 
@@ -62,5 +70,6 @@ export {
   UbuntuExpansion,
 
   // mysql
-  ForgetPassword
+  ForgetPassword,
+
 }
